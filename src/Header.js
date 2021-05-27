b73a0aa074c7423b44f216c5f45096d3742bd935
Sergey <levashov04sergey@gmail.com>
2021-05-27 23:46:02 +0300

    login problem

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,18 @@ import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import {useDispatch} from "react-redux";
+import {auth} from "./firebase";
+import {logout} from "./features/userSlice";
 
 function Header() {
+    const dispatch = useDispatch();
+
+    const logoutOfApp = () => {
+        dispatch(logout());
+        auth.signOut();
+    };
+
     return (
         <div className="header">
             <div className="header__left">
@@ -18,7 +28,10 @@ function Header() {
 
                 <div className="header__search">
                     <SearchIcon/>
-                    <input type="text"/>
+                    <input
+                        placeholder="Search"
+                        type="text"
+                    />
                 </div>
             </div>
             <div className="header__right">
@@ -30,21 +43,22 @@ function Header() {
                     Icon={SupervisorAccountIcon}
                     title='My Network'
                 />
-                 <HeaderOption
+                <HeaderOption
                     Icon={BusinessCenterIcon}
                     title='Jobs'
                 />
-                 <HeaderOption
+                <HeaderOption
                     Icon={ChatIcon}
                     title='Messaging'
                 />
-                 <HeaderOption
+                <HeaderOption
                     Icon={NotificationsIcon}
                     title='Notifications'
                 />
                 <HeaderOption
                     avatar="https://media-exp1.licdn.com/dms/image/C5603AQFUQ6ey57udmg/profile-displayphoto-shrink_100_100/0/1516848359102?e=1627516800&v=beta&t=A8t_k3oFGDrNQIPsmsNsBaTrwpCszwecTZIDmOpDJ2E"
                     title="me"
+                    onClick={logoutOfApp}
                 />
 
             </div>
